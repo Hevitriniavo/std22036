bdeba893d27201bcef6f64b2a41231d2250a8dd8
Hevitriniavo <hei.tantely@gmail.com>
2025-02-05 07:58:51 +0300

    chore: endpoint /api/hello

---
+++ src/main/java/com/fresh/coding/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.fresh.coding.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/api/hello")
+    public Map<String, String> sayHello() {
+        return Map.of("message", "Hello World");
+    }
+}
