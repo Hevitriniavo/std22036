45f83c3d3276f1c33d41b7bc1483ab2412f87c46
Hevitriniavo <hei.tantely@gmail.com>
2025-02-05 08:15:58 +0300

    chore: format HelloWorldController

--- src/main/java/com/fresh/coding/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/fresh/coding/endpoint/rest/controller/HelloWorldController.java
@@ -1,15 +1,14 @@
 package com.fresh.coding.endpoint.rest.controller;
 
+import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/api/hello")
-    public Map<String, String> sayHello() {
-        return Map.of("message", "Hello World");
-    }
+  @GetMapping("/api/hello")
+  public Map<String, String> sayHello() {
+    return Map.of("message", "Hello World");
+  }
 }
